c80c87c9fcab6fc70ed6740791abb5d2181a87df
Hitesh Ambarkhane <hiteshtheone@gmail.com>
2014-03-16 19:54:10 +0000

    onchange + switch

--- index.php
+++ index.php
@@ -4,42 +4,42 @@
 <title>groupdeal.com</title>
 <link rel="stylesheet" type="text/css" href="mainpage.css"> 
 <script>
-function changeOption()
-{
-alert("hitesh");
-    var selectBox = document.getElementById("myOption");
-    var selectedValue = selectBox.options[selectBox.selectedIndex].value;
-    alert(selectedValue);
-    switch(selectedValue)
-{
-case 1:
-myTravel();
-break;
-case 2:
-myServices();
-break;
-case 3:
-myProducts();
-case 4:
-myOffers()
-break;
-}
-}
 function myTravel()
 {
-document.getElementById("results").innerHTML="Travel data streamed here!!!";
+	document.getElementById("results").innerHTML="Travel data streamed here!!!";
 }
 function myProducts()
 {
-document.getElementById("results").innerHTML="Products data streamed here!!!";
+	document.getElementById("results").innerHTML="Products data streamed here!!!";
 }
 function myServices()
 {
-document.getElementById("results").innerHTML="Services data streamed here!!!";
+	document.getElementById("results").innerHTML="Services data streamed here!!!";
 }
 function myOffers()
 {
-document.getElementById("results").innerHTML="Offers data streamed here!!!";
+	document.getElementById("results").innerHTML="Offers data streamed here!!!";
+}
+function changeOption()
+{
+	alert("hitesh");
+	var selectBox = document.getElementById("myOption");
+	var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+	alert(selectedValue);
+	switch(selectedValue)
+	{
+		case 1:
+			myTravel();
+			break;
+		case 2:
+			myServices();
+			break;
+		case 3:
+			myProducts();
+		case 4:
+			myOffers()
+				break;
+	}
 }
 </script>
 </head> 
